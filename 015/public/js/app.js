2ef22de796e030206aeadcbda0c8b09872e2a12f
Tomohide Takao <dorayaki.tomo@gmail.com>
2015-07-20 17:33:00 +0900

    Add a Bump Map Sample.

--- 015/public/js/app.js
+++ 015/public/js/app.js
@@ -1,132 +1,238 @@
-/* global RenderingContext */
 /* global mat4 */
 ( function() {
   function main() {
-    var context = new RenderingContext( 'canvas' );
+    var c  = document.getElementById( 'canvas' );
+    var gl = c.getContext( 'webgl' ) || c.getContext( 'experimental-webgl' );
 
-    context.createProgram( [ 'vs', 'fs' ] );
+    var canvasSize = Math.min( this.innerWidth, this.innerHeight );
 
-    context.bindVbos([
-      { name: 'positions', vertices: positions, stride: 3 },
-      { name: 'colors',    vertices: colors, stride: 4 },
-      { name: 'normals',   vertices: normals, stride: 3 },
-    ]);
+    c.width  = canvasSize;
+    c.height = canvasSize;
 
-    context.bindIbo( indexes );
+    var tex = gl.createTexture();
 
-    context.enableDepthTest();
+    initTexture( '../img/normalmap.png', tex, initRender );
 
-    var count = 0;
-    render();
+    function initTexture( path, texture, fn ) {
+      var img = new Image();
+      img.onload = function(){
+        gl.bindTexture( gl.TEXTURE_2D, texture );
+        gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT );
+        gl.generateMipmap( gl.TEXTURE_2D );
+        gl.bindTexture( gl.TEXTURE_2D, null );
+        if( fn !== null ){
+          fn( texture );
+        }
+      };
+      img.src = path;
+    }
 
+    function initRender( texture ) {
+      var vs = createShader( 'vs' );
+      var fs = createShader( 'fs' );
 
-    function render() {
-      count++;
+      if ( !vs || !fs ) {
+        return;
+      }
 
-      var deg = count % 360;
-      var rad = deg * Math.PI / 180;
+      var program = createProgram( [ vs, fs ] );
 
-      var mMatrix   = mat4.identity( mat4.create() );
-      var vMatrix   = mat4.identity( mat4.create() );
-      var pMatrix   = mat4.identity( mat4.create() );
-      var vpMatrix  = mat4.identity( mat4.create() );
-      var mvpMatrix = mat4.identity( mat4.create() );
+      var locations = new Array( 4 );
+      locations[0]  = gl.getAttribLocation( program, 'position' );
+      locations[1]  = gl.getAttribLocation( program, 'color' );
+      locations[2]  = gl.getAttribLocation( program, 'normal' );
+      locations[3]  = gl.getAttribLocation( program, 'textureCoords' );
 
-      var fovy = 45;
-      var cx   = 1 * Math.sin( 0 );
-      var cz   = 1 * Math.cos( 0 );
+      var strides = [ 3, 4, 3, 2 ];
 
-      var lightDirection = [ 0.0, 0.25, 0.75 ];
-      var eyePosition    = [ cx, 0.0, cz ];
-      var centerPosition = [ 0.0, 0.0, 0.0 ];
-      var cameraUp       = [ 0.0, 1.0, 0.0 ];
-      var ambientColor   = [ 0.5, 0.1, 0.1, 0.0 ];
+      var position = [
+        -0.5,  0.5,  0.0,// 0 left
+         0.5,  0.5,  0.0,// 1 top
+        -0.5, -0.5,  0.0,// 2 center
+         0.5, -0.5,  0.0,// 3 bottom
+      ];
 
-      // ビュー座標変換
-      mat4.lookAt( vMatrix, eyePosition, centerPosition, cameraUp );
-      // 投影変換・クリッピング
-      mat4.perspective( pMatrix, fovy, 1, 0.1, 100.0 );
+      // 色情報、左から順にRGBA
+      var color = [
+        1.0, 0.0, 0.0, 1.0,// 0
+        0.0, 1.0, 0.0, 1.0,// 1
+        0.0, 0.0, 1.0, 1.0,// 2
+        1.0, 1.0, 1.0, 1.0,// 3
+      ];
 
-      mat4.rotateY( mMatrix, mMatrix, rad );
+      var normal = [
+        0.0, 0.0, 1.0,// 0
+        0.0, 0.0, 1.0,// 1
+        0.0, 0.0, 1.0,// 2
+        0.0, 0.0, 1.0,// 3
+      ];
 
-      // かける順番に注意
-      mat4.multiply( vpMatrix, pMatrix, vMatrix );
-      mat4.multiply( mvpMatrix, vpMatrix, mMatrix );
+      var textureCoords = [
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0
+      ];
 
-      var invMatrix = mat4.identity( mat4.create() );
-      mat4.invert( invMatrix, mMatrix );
+      // vboの作成
+      var positionVbo  = createVbo( position );
+      gl.bindBuffer( gl.ARRAY_BUFFER, positionVbo );
+      gl.enableVertexAttribArray( locations[0] );
+      gl.vertexAttribPointer( locations[0], strides[0], gl.FLOAT, false, 0, 0 );
 
-      context.bindUniforms( [
-        { name: 'mvpMatrix',      type: 'matrix4fv', value: mvpMatrix },
-        { name: 'invMatrix',      type: 'matrix4fv', value: invMatrix },
-        { name: 'lightDirection', type: '3fv',       value: lightDirection },
-        { name: 'eyePosition',    type: '3fv',       value: eyePosition },
-        { name: 'centerPoint',    type: '3fv',       value: centerPosition },
-        { name: 'ambientColor',   type: '4fv',       value: ambientColor },
-      ]);
-      
-      context.clear();
+      var colorVbo = createVbo( color );
+      gl.bindBuffer( gl.ARRAY_BUFFER, colorVbo );
+      gl.enableVertexAttribArray( locations[1] );
+      gl.vertexAttribPointer( locations[1], strides[1], gl.FLOAT, false, 0, 0 );
 
-      context.drawElements( context.gl.TRIANGLES, indexes.length );
-      
-      requestAnimationFrame( render );
+      var normalVbo = createVbo( normal );
+      gl.bindBuffer( gl.ARRAY_BUFFER, normalVbo );
+      gl.enableVertexAttribArray( locations[2] );
+      gl.vertexAttribPointer( locations[2], strides[2], gl.FLOAT, false, 0, 0 );
+
+      var textureVbo = createVbo( textureCoords );
+      gl.bindBuffer( gl.ARRAY_BUFFER, textureVbo );
+      gl.enableVertexAttribArray( locations[3] );
+      gl.vertexAttribPointer( locations[3], strides[3], gl.FLOAT, false, 0, 0 );
+
+      // iboの作成
+      var indexes = [
+        0, 1, 2,
+        3, 2, 1
+      ];
+
+      var ibo = gl.createBuffer();
+      gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, ibo );
+      gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Int16Array( indexes ), gl.STATIC_DRAW );
+      gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, null );
+      gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, ibo );
+
+      gl.enable( gl.DEPTH_TEST );
+      gl.depthFunc( gl.LEQUAL );
+
+      var count = 0;
+      render();
+
+      function createShader( id ) {
+        var shaderSrouce = document.getElementById( id );
+        var shader;
+
+        if ( !shaderSrouce ) {
+          console.error( '指定された要素が存在しません' );
+          return;
+        }
+
+        switch( shaderSrouce.type ){
+        case 'x-shader/x-vertex':
+          shader = gl.createShader( gl.VERTEX_SHADER );
+          break;
+        case 'x-shader/x-fragment':
+          shader = gl.createShader( gl.FRAGMENT_SHADER );
+          break;
+        default :
+          return;
+        }
+
+        gl.shaderSource( shader, shaderSrouce.text );
+        gl.compileShader( shader );
+        if ( gl.getShaderParameter( shader, gl.COMPILE_STATUS ) ){
+          return shader;
+        } else {
+          console.error( gl.getShaderInfoLog( shader ) );
+        }
+      }
+
+      function createProgram( shaders ) {
+        var program = gl.createProgram();
+
+        shaders.forEach( function( shader ){ gl.attachShader( program, shader ); });
+        gl.linkProgram( program );
+        if( gl.getProgramParameter( program, gl.LINK_STATUS ) ){
+          gl.useProgram( program );
+          return program;
+        }else{
+          console.error( gl.getProgramInfoLog( program ) );
+        }
+      }
+
+      function createVbo( data ) {
+        var vbo = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, vbo );
+        gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( data ), gl.STATIC_DRAW );
+        gl.bindBuffer( gl.ARRAY_BUFFER, null );
+        return vbo;
+      }
+
+      function render() {
+        count++;
+
+        var deg = count % 360;
+        var rad = deg * Math.PI / 180;
+
+        var mMatrix   = mat4.identity( mat4.create() );
+        var vMatrix   = mat4.identity( mat4.create() );
+        var pMatrix   = mat4.identity( mat4.create() );
+        var vpMatrix  = mat4.identity( mat4.create() );
+        var mvpMatrix = mat4.identity( mat4.create() );
+
+        var fovy = 45;
+        var cx   = 1 * Math.sin( 0 );
+        var cz   = 1 * Math.cos( 0 );
+
+        var lightDirection = [ 0.0, 0.25, 0.75 ];
+        var eyePosition    = [ cx, 0.0, cz ];
+        var centerPosition = [ 0.0, 0.0, 0.0 ];
+        var ambientColor   = [ 0.5, 0.1, 0.1, 0.0 ];
+
+        // ビュー座標変換
+        mat4.lookAt( vMatrix, eyePosition, centerPosition, [ 0.0, 1.0, 0.0 ] );
+        // 投影変換・クリッピング
+        mat4.perspective( pMatrix, fovy, 1, 0.1, 100.0 );
+
+        mat4.rotateY( mMatrix, mMatrix, rad );
+
+        // かける順番に注意
+        mat4.multiply( vpMatrix, pMatrix, vMatrix );
+        mat4.multiply( mvpMatrix, vpMatrix, mMatrix );
+
+        var uLocations = new Array( 6 );
+        uLocations[0]  = gl.getUniformLocation( program, 'mMatrix' );
+        uLocations[1]  = gl.getUniformLocation( program, 'mvpMatrix' );
+        uLocations[2]  = gl.getUniformLocation( program, 'invMatrix' );
+        uLocations[3]  = gl.getUniformLocation( program, 'lightDirection' );
+        uLocations[4]  = gl.getUniformLocation( program, 'eyePosition' );
+        uLocations[5]  = gl.getUniformLocation( program, 'normalMapTexture' );
+
+        gl.uniformMatrix4fv( uLocations[0], false, mMatrix );
+
+        gl.uniformMatrix4fv( uLocations[1], false, mvpMatrix );
+
+        var invMatrix = mat4.identity( mat4.create() );
+        mat4.invert( invMatrix, mMatrix );
+
+        gl.activeTexture( gl.TEXTURE0 );
+        gl.bindTexture( gl.TEXTURE_2D, texture );
+        gl.uniformMatrix4fv( uLocations[2], false, invMatrix );
+
+        gl.uniform3fv( uLocations[3], lightDirection );
+        gl.uniform3fv( uLocations[4], eyePosition );
+        gl.uniform1i( uLocations[5], 0 );
+
+        gl.clearColor( 0.3, 0.3, 0.3, 1.0 );
+        gl.viewport( 0, 0, c.width, c.height );
+        gl.clearDepth( 1.0 );
+        gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+        gl.drawElements( gl.TRIANGLES, indexes.length, gl.UNSIGNED_SHORT, 0 );
+        gl.flush();
+
+        requestAnimationFrame( render );
+      }
     }
   }
-  
-    var positions = [
-    -0.5,  0.0,  0.0,// 0
-     0.0,  0.5,  0.0,// 1
-     0.0,  0.0,  0.5,// 2
-     0.0, -0.5,  0.0,// 3
-     0.5,  0.0,  0.0,// 4
 
-     0.5,  0.0,  0.0,// 5
-     0.0,  0.5,  0.0,// 6
-     0.0,  0.0, -0.5,// 7
-     0.0, -0.5,  0.0,// 8
-    -0.5,  0.0,  0.0 // 9
-  ];
+  this.addEventListener( 'load', main );
 
-  // 色情報、左から順にRGBA
-  var colors = [
-    0.0, 0.0, 1.0, 1.0,// 0
-    0.0, 0.0, 1.0, 1.0,// 1
-    0.0, 0.0, 1.0, 1.0,// 2
-    0.0, 0.0, 1.0, 1.0,// 3
-    0.0, 0.0, 1.0, 1.0,// 4
-
-    0.0, 0.0, 1.0, 1.0,// 5
-    0.0, 0.0, 1.0, 1.0,// 6
-    0.0, 0.0, 1.0, 1.0,// 7
-    0.0, 0.0, 1.0, 1.0,// 8
-    0.0, 0.0, 1.0, 1.0 // 9
-  ];
-
-  var normals = [
-   -1.0, 0.0, 0.0,// 0
-    0.0, 1.0, 0.0,// 1
-    0.0, 0.0, 1.0,// 2
-    0.0,-1.0, 0.0,// 3
-    1.0, 0.0, 0.0,// 4
-
-    1.0, 0.0, 0.0,// 5
-    0.0, 1.0, 0.0,// 6
-    0.0, 0.0,-1.0,// 7
-    0.0,-1.0, 0.0,// 8
-   -1.0, 0.0, 0.0,// 9
-  ];
-
-  var indexes = [
-    0, 1, 2,
-    0, 2, 3,
-    2, 1, 4,
-    2, 4, 3,
-
-    5, 6, 7,
-    5, 7, 8,
-    7, 6, 9,
-    7, 9, 8
-  ];
-  
-  window.addEventListener( 'load', main );
-})();
+} )();
